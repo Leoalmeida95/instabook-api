7ffba06289e311653a73e21c673a080f8a734487
Leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-04-19 03:35:39 -0300

    [add] login usuario

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
-from resources.usuario import Usuario, UsuarioRegistro
+from resources.usuario import Usuario, UsuarioRegistro, UsuarioLogin
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'DontTellAnyone' #pode ser qualque string
 api = Api(app)
+jwt = JWTManager(app)
 
 @app.before_first_request
 def cria_banco():
@@ -15,7 +18,8 @@ def cria_banco():
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 api.add_resource(Usuario, '/usuarios/<int:user_id>')
-api.add_resource(UsuarioRegistro, '/usuarios/cadastro')
+api.add_resource(UsuarioRegistro, '/usuario/cadastro')
+api.add_resource(UsuarioLogin, '/usuario/login')
 
 if __name__ == '__main__':
     from sql_alchemy import banco
--- resources/usuario.py
+++ resources/usuario.py
@@ -1,5 +1,11 @@
 from flask_restful import Resource, reqparse
 from models.usuarioModel import UsuarioModel
+from flask_jwt_extended import create_access_token
+from werkzeug.security import safe_str_cmp
+
+atributos = reqparse.RequestParser()
+atributos.add_argument('login', type=str, required=True, help='The field "login" cannot be left blank.')
+atributos.add_argument('senha', type=str, required=True, help='The field "senha" cannot be left blank.')
 
 class Usuario(Resource):
     #/usuario/{user_id}
@@ -25,9 +31,6 @@ class Usuario(Resource):
 class UsuarioRegistro(Resource):
     #/cadastro
     def post(self):
-        atributos = reqparse.RequestParser()
-        atributos.add_argument('login', type=str, required=True, help='The field "login" cannot be left blank.')
-        atributos.add_argument('senha', type=str, required=True, help='The field "senha" cannot be left blank.')
         dados = atributos.parse_args()
 
         if UsuarioModel.find_by_login(dados['login']):
@@ -36,3 +39,15 @@ class UsuarioRegistro(Resource):
         user = UsuarioModel(**dados)
         user.save()
         return {'message':'User created!'}, 201
+
+class UsuarioLogin(Resource):
+    #login
+    @classmethod
+    def post(cls):
+        dados = atributos.parse_args()
+        user = UsuarioModel.find_by_login(dados['login'])
+        if user and safe_str_cmp(user.senha, dados['senha']):
+            token_acesso = create_access_token(identity=user.user_id)
+            return {'access_token': token_acesso}, 200
+        
+        return {'message': 'User not find.'}, 401
